9112fc480c515efa5927e153a292bf55da349335
indexnoname <indexnoname@proton.me>
2024-07-28 13:17:48 +0300

    no need for venv it will be setup on first time running

--- startup.sh
+++ startup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# create virtual environment if there is none
+if [ ! -d botvenv ]; then
+    python -m venv botvenv
+fi
 
 # Activate the virtual environment
 source botvenv/bin/activate
+
+# Check if discord.py is installed, if not, install it
+if ! python -c "import discord" &> /dev/null; then
+    pip3 install discord.py
+fi
 
 # Run the bot script
 python3 bot.py
