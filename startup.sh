c5ed989a703587815fb850527be0f2f3570d463e
indexnoname <indexnoname@proton.me>
2024-07-28 15:09:55 +0300

    +autoupdater f dont want just comment it

--- startup.sh
+++ startup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
+# make this a coment if dont want autoupdate
+git pull https://github.com/indexnoname/bot-lite.git
+
+
 # create virtual environment if there is none
 if [ ! -d botvenv ]; then
     python3 -m venv botvenv
 fi
 
-# Activate the virtual environment
+# activate the virtual environment
 source botvenv/bin/activate
 
-# Check if discord.py is installed, if not, install it
+# check if discord.py is installed, if not, install it
 if ! python -c "import discord" &> /dev/null; then
     pip3 install discord.py
 fi
 
-# Run the bot script
+# run the bot script
 python3 bot.py
