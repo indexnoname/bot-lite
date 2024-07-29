2df58bfb5f461a303878819e0ffa98ed3c054875
indexnoname <indexnoname@proton.me>
2024-07-30 00:28:06 +0300

    sh update now compact

--- startup.sh
+++ startup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# make this a coment if dont want autoupdate
+# make this a comment if you don't want autoupdate
 git pull https://github.com/indexnoname/bot-lite.git
-
 
 # create virtual environment if there is none
 if [ ! -d botvenv ]; then
@@ -12,22 +11,21 @@ fi
 # activate the virtual environment
 source botvenv/bin/activate
 
-# check if discord.py is installed, if not, install it
-if ! python -c "import discord" &> /dev/null; then
-    pip3 install discord.py
-fi
-if ! python -c "import numpy as np" &> /dev/null; then
-    pip3 install numpy
-fi
-if ! python -c "from PIL import image" &> /dev/null; then
-    pip3 install Pillow
-fi
-if ! python -c "import pymsch" &> /dev/null; then
-    pip3 install pymsch
-fi
-if ! python -c "import pyperclip" &> /dev/null; then
-    pip3 install pyperclip
-fi
+# list of required packages
+REQUIRED_PACKAGES=(
+    discord.py
+    numpy
+    Pillow
+    pymsch
+    pyperclip
+)
+
+# install missing packages
+for package in "${REQUIRED_PACKAGES[@]}"; do
+    if ! python -c "import ${package%%=*}" &> /dev/null; then
+        pip3 install "$package"
+    fi
+done
 
 # run the bot script
 python3 bot.py
