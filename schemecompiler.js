f7fb6caba08d0d4d67c70b01f1341b1c9685290c
indexnoname <indexnoname@proton.me>
2024-08-19 20:36:46 +0300

    + required compiler for mindustry images as there is no such

---
+++ schemecompiler.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const { Schematic } = require('mindustry-schematic-parser');
+
+fs.readFile('scheme.txt', 'utf8', (err, base64) => {
+    if (err) {
+        console.error('Error reading scheme:', err);
+        process.exit(1);
+    }
+
+    try {
+        const schematic = Schematic.decode(base64);
+
+        // Save schematic info to a JSON file
+        const schematicInfo = {
+            name: schematic.name,
+            description: schematic.description,
+        };
+        fs.writeFileSync('/home/nonamecoding/Desktop/bots/json/scheme_info.json', JSON.stringify(schematicInfo, null, 2));
+
+        schematic
+            .render({
+                background: true // enable background
+            })
+            .then(nodeCanvas => nodeCanvas.toBuffer())
+            .then(buffer => fs.writeFileSync('scheme.png', buffer))
+            .then(() => process.exit(0))
+            .catch(err => {
+                console.error('Error rendering scheme:', err);
+                process.exit(1);
+            });
+    } catch (err) {
+        console.error('Error decoding scheme:', err);
+        process.exit(1);
+    }
+});
