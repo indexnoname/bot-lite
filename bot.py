6b9ebf21b342fe3c749e681f5918b38e69785f03
indexnoname <indexnoname@proton.me>
2024-08-19 11:45:06 +0300

    a bit of improvement

--- bot.py
+++ bot.py
@@ -163,13 +163,13 @@ async def convert(ctx, scale: int = 100, resample_method: str = 'LANCZOS'):
     imageio = io.BytesIO(await ctx.message.attachments[0].read())
 
     with Image.open(imageio).convert('RGB') as image:
-        resized_image, new_width, new_height = resize_image(image, scale, resample_method.upper())
-        scheme_file = convert_image_to_scheme(resized_image, ctx.message.attachments[0].filename)
+        image = resize_image(image, scale, resample_method.upper())
+        image = convert_image_to_scheme(resized_image, ctx.message.attachments[0].filename)
         
-        await ctx.send(file=discord.File(fp=scheme_file, filename="scheme.msch"))
+        await ctx.send(file=discord.File(fp=image, filename="scheme.msch"))
 
     # Manually delete variables to free memory
-    del imageio, image, resized_image, scheme_file
+    del imageio, image
 
     # Trigger garbage collection
     gc.collect()
